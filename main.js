5f9e2144df6f1e9fe76274021ab55653e0965751
Bhaskar <89502129+Maxikun@users.noreply.github.com>
2023-05-10 15:56:25 +0530

    new changes

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ function updateElement() {
     if(count > 10)
     {
         count = 0;
-        messageElement.innerHTML = "You have exceeded the limit!!";
+        messageElement.innerHTML = "You have exceeded the limit!! Fuck Off You Feak!!";
         return;
     }
     messageElement.innerHTML = "You have clicked the button " + count + " times.";
