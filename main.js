73af06e15ca032fe6a827ba62c4f1fc9f33b01bb
Bhaskar <89502129+Maxikun@users.noreply.github.com>
2023-10-02 13:40:47 +0530

    polished the website

--- main.js
+++ main.js
@@ -1,19 +1,3 @@
-// main.js
-let count = 0;
-function updateElement() {
-    count++;
-    var messageElement = document.getElementById("message");
-    if(count > 10)
-    {
-        count = 0;
-        messageElement.innerHTML = "You have exceeded the limit!! Fuck Off You Freak!!";
-        return;
-    }
-    messageElement.innerHTML = "You have clicked the button " + count + " times.";
-}
-// main.js
-// main.js
-
 function showRandomImage() {
     let image = document.getElementById("randomImage");
     fetch("https://api.thecatapi.com/v1/images/search")
@@ -21,4 +5,6 @@ function showRandomImage() {
     .then(json => image.src = json[0].url)
 }
 
-
+window.onload = function() {
+    this.showRandomImage();
+}
